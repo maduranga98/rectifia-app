b457612d13c8d200acc6ea061dc927dd537d33c7
maduranga98 <madurangalakbima@gmail.com>
2026-08-02 17:08:50 +0530

    .env data

--- src/config/env.js
+++ src/config/env.js
@@ -8,7 +8,6 @@ export const env = {
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
   },
-  useEmulators: import.meta.env.VITE_USE_FIREBASE_EMULATORS === 'true',
-}
+  useEmulators: import.meta.env.VITE_USE_FIREBASE_EMULATORS === "true",
+};
